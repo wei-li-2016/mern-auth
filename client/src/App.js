159a9c0592f17b3be170f05e715005716b6fbc8a
PineGolden311 <software.developer112@gmail.com>
2019-05-05 10:08:17 -0600

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import './App.css';
 
 import { Provider } from "react-redux";
@@ -9,6 +12,28 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import PrivateRoute from './components/private-route/PrivateRoute';
+import Dashboard from './components/dashboard//Dashboard';
+
+if (localStorage.jwtToken) {
+  //Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  //Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  //Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = './login';
+  }
+}
 
 function App() {
   return (
@@ -19,6 +44,9 @@ function App() {
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
         </div>
       </Router>
     </Provider>
